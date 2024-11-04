b721f565d7ab495849c08533bc6b3c429616cd57
Suvonov2000 <suvonov200@gmail.com>
2024-11-04 10:28:30 +0900

    JS Table

--- index.js
+++ index.js
@@ -1,1 +1,96 @@
-console.log(document.getElementById("single"));
+var restoreButton = document.querySelector(".restoreData");
+
+var data = [
+  {
+    id: 1,
+    name: "Umidjon",
+    surname: "Suvonov",
+    age: "24",
+  },
+  {
+    id: 2,
+    name: "Asadbek",
+    surname: "Abduvoitov",
+    age: "19",
+  },
+  {
+    id: 3,
+    name: "Muhammad",
+    surname: "Hamraev",
+    age: "20",
+  },
+  {
+    id: 4,
+    name: "Husan",
+    surname: "Bo'taev",
+    age: "23",
+  },
+];
+
+var tbodyElements = "";
+
+const deleteUser = (id) => {
+  console.log(id);
+  data = data.filter((value) => {
+    return value.id !== id;
+  });
+  tbodyElements = "";
+  renderingData();
+};
+
+const renderingData = () => {
+  if (data.length == 0) {
+    restoreButton.style.display = "block";
+  } else {
+    restoreButton.style.display = "none";
+  }
+  data.map((value) => {
+    tbodyElements += `
+    <tr>
+       <td>${value.id}</td>
+       <td>${value.name}</td>
+       <td>${value.surname}</td>
+       <td>${value.age}</td>
+       <td>
+     <button onclick="deleteUser(${value.id})" >Delete</button>
+    </td>
+    </tr>
+
+    `;
+  });
+  console.log(tbodyElements);
+  document.querySelector("tbody").innerHTML = tbodyElements;
+};
+
+renderingData();
+
+restoreButton.addEventListener("click", () => {
+  data = [
+    {
+      id: 1,
+      name: "Umidjon",
+      surname: "Suvonov",
+      age: "24",
+    },
+    {
+      id: 2,
+      name: "Asadbek",
+      surname: "Abduvoitov",
+      age: "19",
+    },
+    {
+      id: 3,
+      name: "Muhammad",
+      surname: "Hamraev",
+      age: "20",
+    },
+    {
+      id: 4,
+      name: "Husan",
+      surname: "Bo'taev",
+      age: "23",
+    },
+  ];
+
+  renderingData();
+});
